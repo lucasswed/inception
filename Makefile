6eb59253d0517bad9a3f1169e46d1447309d489d
lucasvm <lucas.2610.silva@gmail.com>
2023-11-09 16:54:36 +0000

    all container done, trying to fix mariadb enter without pass

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
-all: clean up
+all: up
 
 up:
+	mkdir -p /home/lucas/data/mysql
+	mkdir -p /home/lucas/data/wordpress
 	docker compose -f ./srcs/docker-compose.yml up -d
 
 down:
@@ -8,5 +10,13 @@ down:
 
 stop:
 	docker compose -f ./srcs/docker-compose.yml stop
+
+clean: down
+		cd /home/lucas/; sudo rm -rf data;
+
+logs:
+	docker compose -f ./srcs/docker-compose.yml logs
+
+re: clean up
 
 .PHONY: up down stop clean
--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -4,11 +4,25 @@ if [ ! -d "/var/lib/mysql/$DB_NAME" ]; then
 
 /etc/init.d/mysql start;
 
-sleep 1;
+mysql_secure_installation <<_EOF_
+
+Y
+$DB_ADMIN
+$DB_PASS
+Y
+n
+Y
+Y
+_EOF_
+
+sleep 3;
+
+mysql -u root -e "DELETE FROM mysql.user WHERE User='';"
+mysql -u root -e 
 
 echo "Creating admin user"
 mysql -u root -e "CREATE DATABASE $DB_NAME;"
-mysql -u root -e "CREATE USER '$DB_ADMIN'@'%' IDENTIFIED BY '$DB_ADMIN_PASS';"
+mysql -u root -e "CREATE USER '$DB_ADMIN'@'%' IDENTIFIED BY '$DB_PASS';"
 mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_ADMIN'@'%';"
 mysql -u root -e "FLUSH PRIVILEGES;"
 
