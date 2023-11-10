676056d462a4d951a22696f9242c7522249ef151
lucas-ma <lucas-ma@lucas-ma.lucas-ma.42.fr>
2023-11-10 19:06:50 +0000

    fixing the mariadb access with no pass

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 all: up
 
 up:
-	mkdir -p /home/lucas/data/mysql
-	mkdir -p /home/lucas/data/wordpress
+	mkdir -p /home/lucas-ma/data/mysql
+	mkdir -p /home/lucas-ma/data/wordpress
 	docker compose -f ./srcs/docker-compose.yml up -d
 
 down:
@@ -12,7 +12,7 @@ stop:
 	docker compose -f ./srcs/docker-compose.yml stop
 
 clean: down
-		cd /home/lucas/; sudo rm -rf data;
+		cd /home/lucas-ma/; sudo rm -rf data;
 
 logs:
 	docker compose -f ./srcs/docker-compose.yml logs
--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -4,17 +4,6 @@ if [ ! -d "/var/lib/mysql/$DB_NAME" ]; then
 
 /etc/init.d/mysql start;
 
-mysql_secure_installation <<_EOF_
-
-Y
-$DB_ADMIN
-$DB_PASS
-Y
-n
-Y
-Y
-_EOF_
-
 sleep 3;
 
 echo "Creating admin user"
@@ -22,6 +11,13 @@ mysql -u root -e "CREATE DATABASE $DB_NAME;"
 mysql -u root -e "CREATE USER '$DB_ADMIN'@'%' IDENTIFIED BY '$DB_PASS';"
 mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_ADMIN'@'%';"
 mysql -u root -e "FLUSH PRIVILEGES;"
+
+/etc/init.d/mysql stop;
+else
+
+/etc/init.d/mysql start;
+
+sleep 3;
 
 /etc/init.d/mysql stop;
 fi
