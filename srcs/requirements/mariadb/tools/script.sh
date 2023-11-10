83f97ebb40349aa0635d1106f6b74af985d4088f
lucas-ma <lucas-ma@lucas-ma.lucas-ma.42.fr>
2023-11-10 20:28:58 +0000

    test

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -6,11 +6,15 @@ if [ ! -d "/var/lib/mysql/$DB_NAME" ]; then
 
 sleep 3;
 
+
 echo "Creating admin user"
 mysql -u root -e "CREATE DATABASE $DB_NAME;"
 mysql -u root -e "CREATE USER '$DB_ADMIN'@'%' IDENTIFIED BY '$DB_PASS';"
 mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_ADMIN'@'%';"
 mysql -u root -e "FLUSH PRIVILEGES;"
+
+echo "Setting password for root user"
+mysqladmin -u root password "$DB_PASS"
 
 /etc/init.d/mysql stop;
 else
@@ -21,5 +25,6 @@ sleep 3;
 
 /etc/init.d/mysql stop;
 fi
+#mariadb-data  | Usage: /etc/init.d/mysql start|stop|restart|reload|force-reload|status|bootstrap
 
 exec "$@"
